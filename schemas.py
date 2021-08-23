03c0edbf2f24b47a577fd6f2a4b6bce4aefae1f8
jhon0210 <jhondavidr56@gmail.com>
2021-08-22 20:36:22 -0500

    add: adicionando archivo de schemas para capturar y mostrar variables del modelo dog

---
+++ schemas.py
@@ -0,0 +1,19 @@
+from pydantic import BaseModel
+from typing import Optional
+import datetime
+
+
+class Dog(BaseModel):
+    name: str
+    is_adopted: bool
+
+
+class ShowDog(BaseModel):
+    name: str
+    picture: str
+    is_adopted: bool
+    create_date: datetime.datetime
+
+    class Config:
+        orm_mode = True
+
