4eea0aff8c552bb427428232c9c93dbe1faa8c72
jhon0210 <jhondavidr56@gmail.com>
2021-08-22 20:44:37 -0500

    add: adicionando logica para consumo de servicio de imagenes de dogs y modificando consulta de la funcion list_dogs

--- repository/dog.py
+++ repository/dog.py
@@ -1,23 +1,24 @@
 from fastapi import HTTPException, Response, status
 from sqlalchemy.orm import Session
+import requests
 
 from .. import models, schemas
 
-def list_dogs(limit: int, is_adopted: bool, db: Session):
-    if is_adopted == None:
-        dogs = db.query(models.Dog).limit(limit).all()
-    else:
-        dogs = db.query(models.Dog) \
-                .where(models.Dog.is_adopted == is_adopted) \
-                .limit(limit).all()
+DOGS_PICTURES_API = 'https://dog.ceo/api/breeds/image/random'
+
+def list_dogs(limit: int, db: Session):
+    dogs = db.query(models.Dog).limit(limit).all()
 
     return dogs
 
 
 def create(req: schemas.Dog, db: Session):
+    response = requests.get(DOGS_PICTURES_API)
+    picture = response.json()['message']
+
     new_dog = models.Dog(
         name = req.name,
-        picture = req.picture,
+        picture = picture,
         is_adopted = req.is_adopted,
     )
 
