f6f584dd58719d294939920ef1bac6bc1b60ec69
jhon0210 <jhondavidr56@gmail.com>
2021-08-23 16:34:27 -0500

    add: adiciona linea para importar modelos user y autenticacion

--- main.py
+++ main.py
@@ -2,12 +2,13 @@ from fastapi import FastAPI
 
 from . import models
 from .database import engine
-from .routers import dog, user
+from .routers import dog, user, auth
 
 app = FastAPI()
 
 models.Base.metadata.create_all(engine)
 
+app.include_router(auth.router)
 app.include_router(dog.router)
 app.include_router(user.router)
 
