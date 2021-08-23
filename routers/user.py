b1593c765280e7e043fde023b53104929a5266a4
jhon0210 <jhondavidr56@gmail.com>
2021-08-23 14:28:00 -0500

    refactor: se cambia schema showuser a user

--- routers/user.py
+++ routers/user.py
@@ -12,17 +12,17 @@ router = APIRouter(
 )
 
 
-@router.get('/', response_model = List[schemas.ShowUser])
+@router.get('/', response_model = List[schemas.User])
 def list_users(limit: int=10, active: Optional[bool]=None, db: Session=Depends(get_db)):
     return user.list_users(limit, active, db)
 
 
-@router.get('/{id}', status_code=status.HTTP_200_OK, response_model=schemas.ShowUser)
+@router.get('/{id}', status_code=status.HTTP_200_OK, response_model=schemas.User)
 def find_user(id, res: Response, db: Session = Depends(get_db)):
     return user.find(id, res, db)
 
 
-@router.post('/', status_code=status.HTTP_201_CREATED, response_model=schemas.ShowUser)
+@router.post('/', status_code=status.HTTP_201_CREATED, response_model=schemas.User)
 def create_user(req: schemas.User, db: Session=Depends(get_db)):
     return user.create(req, db)
 
