1c52245e756eaabb906c4c05e8bb66885bc9bc4e
jhon0210 <jhondavidr56@gmail.com>
2021-08-23 16:37:30 -0500

    adiciona la estructura para los modelos

--- routers/dog.py
+++ routers/dog.py
@@ -2,7 +2,7 @@ from typing import List, Optional
 from fastapi import APIRouter, Depends, status, Response, HTTPException
 from sqlalchemy.orm import Session
 
-from .. import models, schemas
+from .. import models, oauth2, schemas
 from ..database import get_db
 from ..repository import dog
 
@@ -13,12 +13,18 @@ router = APIRouter(
 
 
 @router.get('/', response_model=List[schemas.ShowDog])
-def list_dogs(limit: int=10, db: Session=Depends(get_db)):
+def list_dogs(
+        limit: int=10,
+        db: Session=Depends(get_db),
+        get_current_user: schemas.User=Depends(oauth2.get_current_user)):
     return dog.list_dogs(limit, db)
 
 
 @router.get('/{name}', status_code=status.HTTP_200_OK, response_model=List[schemas.ShowDog])
-def find_dogs(name, db: Session=Depends(get_db)):
+def find_dogs(
+        name,
+        db: Session=Depends(get_db),
+        get_current_user: schemas.User=Depends(oauth2.get_current_user)):
     return dog.find_matches(name, response)
 
 
@@ -28,16 +34,26 @@ def find_adopted_dog(db: Session=Depends(get_db)):
 
 
 @router.post('/', status_code=status.HTTP_201_CREATED, response_model=schemas.ShowDog)
-def create_dog(req: schemas.Dog, db: Session=Depends(get_db)):
+def create_dog(
+        req: schemas.Dog,
+        db: Session=Depends(get_db),
+        get_current_user: schemas.User=Depends(oauth2.get_current_user)):
     return dog.create(req, db)
 
 
 @router.put('/{id}', status_code=status.HTTP_202_ACCEPTED)
-def update_dog(id, req: schemas.Dog, db: Session=Depends(get_db)):
+def update_dog(
+        id,
+        req: schemas.Dog,
+        db: Session=Depends(get_db),
+        get_current_user: schemas.User=Depends(oauth2.get_current_user)):
     return dog.update(id, req, db)
 
 
 @router.delete('/{id}', status_code=status.HTTP_204_NO_CONTENT)
-def destroy_dog(name, db: Session=Depends(get_db)):
-    pass
+def destroy_dog(
+        id,
+        db: Session=Depends(get_db),
+        get_current_user: schemas.User=Depends(oauth2.get_current_user)):
+    return dog.destroy(id, db)
 
